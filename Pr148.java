2bcf969dee1a00a0e7596e87ddcc69b78a10fa8b
Dinesh  Ahire <ahiredinesh2@gmail.com>
2023-06-13 18:09:23 +0530

    C Programme Problem

---
+++ Pr148.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Pr148
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		String str;
+		
+		System.out.println("Enter your name");
+		str = sobj.nextline();
+		
+		System.out.println("Hello :" +str);
+		
+	}
+}
