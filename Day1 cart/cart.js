535bf25495693185369e297fbd04bfb86612ceb2
Dinesh  Ahire <ahiredinesh2@gmail.com>
2023-06-21 20:09:51 +0530

    Cart  and Payment Page

---
+++ Day1 cart/cart.js
@@ -0,0 +1,7 @@
+document.querySelector("cartbtn").addEventListener("click",function(){
+    document.body.classList.add("xyz");
+});
+
+// document.querySelector("cartbtn").addEventListener("click",function(){
+//     document.body.classList.remove("xyz");
+// });
